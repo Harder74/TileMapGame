e11ab0148def85a7ea1393dbc5ca211290e538b6
Harder74 <tylerdrewharder@ksu.edu>
2022-03-31 11:44:12 -0500

    Add project files.

---
+++ TileMapGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TileMapGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TileMapGame())
+                game.Run();
+        }
+    }
+}
